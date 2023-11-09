a09fb041ae5a43f0f2f3dca640051e40efb2696f
hashizume <hashizume@fabric-arts.co.jp>
2023-11-09 10:46:04 +0900

    [update]入力ボックス実装

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -14,7 +14,7 @@ const SearchManufacturer = ({ manufacturer, setManufacturer }: SearchManufacture
 
   return (
     <div className="search-manufacturer">
-      <Combobox>
+      <Combobox value={manufacturer} onChange={setManufacturer}>
         <div className="relative w-full">
           <Combobox.Button className="absolute top-[14px]">
             <Image src="/car-logo.svg" width={20} height={20} className="ml-4" alt="Car Logo" />
@@ -23,17 +23,16 @@ const SearchManufacturer = ({ manufacturer, setManufacturer }: SearchManufacture
 
           <Transition as={Fragment} leave="trasition ease-in duration-100" leaveFrom="opacity-100" leaveTo="opacity-0" afterLeave={() => setQuery("")}>
             <Combobox.Options>
-              {filteredManufacturers.length === 0 && query !== "" ? (
-                <Combobox.Option value={query} className="search-manufacturer__option">
-                  Create "{query}"
+              {filteredManufacturers.map((item) => (
+                <Combobox.Option key={item} className={({ active }) => `relative search-manufacturer__option ${active ? "bg-primary-blue text-white" : "text-gray-900"}`} value={item}>
+                  {({ selected, active }) => (
+                    <>
+                      <span className={`block truncate ${selected ? "font-medium" : "font-normal"}`}>{item}</span>
+                      {selected ? <span className={`absolute inset-y-0 left-0 flex items-center pl-3 ${active ? "text-white" : "text-pribg-primary-purple"}`}></span> : null}
+                    </>
+                  )}
                 </Combobox.Option>
-              ) : (
-                filteredManufacturers.map((item) => (
-                  <Combobox.Option key={item} className={({ active }) => `relative search-manufacturer__option ${active ? "bg-primary-blue-100 text-white" : "text-gray-900"}`} value={item}>
-                    {item}
-                  </Combobox.Option>
-                ))
-              )}
+              ))}
             </Combobox.Options>
           </Transition>
         </div>
